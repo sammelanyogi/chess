eb3b4b85d59deb5d044b89916f054cf8f188d1fa
Sammelan Yogi <sammelanyogi@gmail.com>
2024-09-07 18:10:02 +0545

    Web test deploy.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,23 @@ mod chessboard;
 mod engine;
 
 use bevy::prelude::*;
+use bevy::window::*;
 
 use camera::CameraPlugin;
 use chessboard::ChessBoardPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, CameraPlugin, ChessBoardPlugin))
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WindowResolution::new(1280., 720.).with_scale_factor_override(1.),
+                    ..default()
+                }),
+                ..default()
+            }),
+            CameraPlugin,
+            ChessBoardPlugin,
+        ))
         .run();
 }
